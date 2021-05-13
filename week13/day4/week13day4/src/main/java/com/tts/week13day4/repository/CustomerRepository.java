ebcd2a5c7ed0fac93450dcb0116fcf6d6f4aa03c
Lydia <15172508+loshun@users.noreply.github.com>
2021-05-13 11:49:21 -0400

    Adding a class

---
+++ week13/day4/week13day4/src/main/java/com/tts/week13day4/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.tts.week13day4.repository;
+
+import com.tts.week13day4.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository <Customer, Long> {
+
+
+}
--- week13/day4/week13day4/src/main/java/com/tts/week13day4/repository/CustomerRepsitory.java
+++
@@ -1,8 +0,0 @@
-package com.tts.week13day4.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepsitory extends CrudRepository <Customer, Long> {
-    
-
-}
