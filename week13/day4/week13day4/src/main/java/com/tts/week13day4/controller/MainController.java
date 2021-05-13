a20b64617bc2bd8c397b2556ae74c5f0f47f81d5
Lydia <15172508+loshun@users.noreply.github.com>
2021-05-13 11:27:05 -0400

    updating notes

--- week13/day4/week13day4/src/main/java/com/tts/week13day4/controller/MainController.java
+++ week13/day4/week13day4/src/main/java/com/tts/week13day4/controller/MainController.java
@@ -1,18 +1,24 @@
 package com.tts.week13day4.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@RestController("/main")
 public class MainController {
 
     // this an endpoint
     // we can access this resource on this endpoint
     // all that we get back is a string
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String home() {
         return "Hello World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String homeWithName(@PathVariable String name) {
+        return "Hello " + name;
     }
 
     //going to an endpoint will resolve the method
@@ -22,4 +28,25 @@ public class MainController {
         return 5 + 4;
     }
 
+    @RequestMapping("/print")
+    public void doPrint() {
+        System.out.println("Hello from the terminal");
+    }
+
+    @GetMapping("/parammath/{x}/{y}")
+    public Integer doMathWithParams(@PathVariable int x,
+                                    @PathVariable int y) {
+        return x +y;
+    }
+
+    // x and y act like placeholders
+    // an end user can put in any legal integer
+    // so that the method resolves
+    // x and y will be used as method arguments
+    // this works because of the annotation @PathVariable
+    // thus, our endpoint resource is dynamic
+    @GetMapping("/math/sub/{x}/{y}")
+    public Integer doSubtraction (@PathVariable int x, @PathVariable int y) {
+        return x - y;
+    }
 }
