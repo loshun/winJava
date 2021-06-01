594f91b7984cd70de36b3cb0cbf8918f8be80efd
Lydia <15172508+loshun@users.noreply.github.com>
2021-06-01 15:57:57 -0400

    Updating Twitter Project

--- week16/day1-3/TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/model/Tweet.java
+++ week16/day1-3/TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/model/Tweet.java
@@ -4,8 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
 
 @Data
 @Builder
@@ -13,5 +19,26 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 @Entity
 public class Tweet {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "tweet_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+//    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+//    @JoinTable(name = "tweet_tag", joinColumns = @JoinColumn(name = "tweet_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
+//    private List<Tag> tags;
+
+    @NotEmpty(message = "Tweet cannot be empty")
+    @Length(max = 280, message = "Tweet cannot have more than 280 characters")
+    private String message;
+
+    @CreationTimestamp
+    private Date createdAt;
+
 }
